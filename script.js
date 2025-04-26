09c7e1d9bd9d9f86c71bba56f4d81d3d6818deb4
F1B1 <F1B1>
2025-04-26 14:02:22 +0300

    page

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-window.addEventListener('DOMContentLoaded',()=>{
+window.addEventListener('DOMContentLoaded', () => {
+  // Проверка и выполнение бургер-меню
+  const burger = document.querySelector('.menu__icon');
+  const header = document.querySelector('.header');
+  const pageBody = document.querySelector('.page__body');
 
-    const pageBody = document.querySelector('.page__body')
-
-    const burger = document.querySelector('.menu__icon')
-    const header = document.querySelector('.header')
-
-    burger.addEventListener("click", function(e) {
-        burger.classList.toggle('active')
-        header.classList.toggle('show') 
-        pageBody.classList.toggle('no-scrolling') 
-    });
-
-    const swiperPrimary = new Swiper('.swiper', {
-        slidesPerView: 'auto',
-        spaceBetween: 8,
-      
-        pagination: {
-          el: '.primary__pagination',
-        },
-      
+  if (burger && header && pageBody) {
+      burger.addEventListener("click", function(e) {
+          burger.classList.toggle('active');
+          header.classList.toggle('show'); 
+          pageBody.classList.toggle('no-scrolling'); 
       });
+  }
 
-})
+  const swiperPrimaryEl = document.querySelector('.primary__swiper');
+  if (swiperPrimaryEl) {
+      const swiperPrimary = new Swiper('.primary__swiper', {
+          slidesPerView: 'auto',
+          spaceBetween: 8,
+          pagination: {
+              el: '.primary__pagination',
+          },
+      });
+  }
+
+  const swiperReviewEl = document.querySelector('.reviews__swiper');
+  if (swiperReviewEl) {
+      const swiperReview = new Swiper('.reviews__swiper', {
+          slidesPerView: 'auto',
+          spaceBetween: 8,
+      });
+  }
+});
